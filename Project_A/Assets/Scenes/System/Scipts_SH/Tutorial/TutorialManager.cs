5a9c4a0c74ab78b3675ff797ec9c503efeea197f
Sugedi <happy981215@naver.com>
2024-01-27 20:28:50 +0900

    New Function: UI 튜토리얼 수정

--- Project_A/Assets/Scenes/System/Scipts_SH/Tutorial/TutorialManager.cs
+++ Project_A/Assets/Scenes/System/Scipts_SH/Tutorial/TutorialManager.cs
@@ -10,7 +10,6 @@ public class TutorialManager : MonoBehaviour
     // 조이스틱 & UI ON/OOF용
     public CanvasGroup joy;
     public CanvasGroup stageUI;
-    public CanvasGroup questIcon;
 
     // 튜토리얼 단계
     // 0은 UI 튜토리얼
@@ -25,6 +24,9 @@ public class TutorialManager : MonoBehaviour
     public CanvasGroup UI_5;
     public CanvasGroup UI_6;
     public CanvasGroup UI_7;
+    public CanvasGroup UI_8;
+    public CanvasGroup UI_9;
+    public CanvasGroup Quest;
 
     public int PanelNum = 0;
 
@@ -49,7 +51,8 @@ public class TutorialManager : MonoBehaviour
         CanvasGroup NO_1 = GameObject.Find("UITutor").transform.Find("UI_0").transform.Find("Image").transform.Find("NO1").GetComponent<CanvasGroup>();
         CanvasGroup NO_2 = GameObject.Find("UITutor").transform.Find("UI_0").transform.Find("Image").transform.Find("NO2").GetComponent<CanvasGroup>();
         CanvasGroup NO_3 = GameObject.Find("UITutor").transform.Find("UI_0").transform.Find("Image").transform.Find("NO3").GetComponent<CanvasGroup>();
-
+        CanvasGroup NO_4 = GameObject.Find("UITutor").transform.Find("UI_2").transform.Find("Image").transform.Find("NO4").GetComponent<CanvasGroup>();
+        CanvasGroup NO_5 = GameObject.Find("UITutor").transform.Find("UI_2").transform.Find("Image").transform.Find("NO5").GetComponent<CanvasGroup>();
         // UI_0
         if (PanelNum == 0)
         {
@@ -87,48 +90,72 @@ public class TutorialManager : MonoBehaviour
         else if(PanelNum == 4)
         {
             CanvasGroupOff(UI_2);
+            CanvasGroupOff(NO_4);
+            CanvasGroupOn(NO_5);
+            CanvasGroupOff(Quest);
             CanvasGroupOn(UI_3);
-            CanvasGroupOff(stageUI);
-            CanvasGroupOff(questIcon);
+
             PanelNum++;
         }
-        // UI_1
+
         else if(PanelNum == 5)
         {
             CanvasGroupOff(UI_3);
-            CanvasGroupOn(UI_4);
+            CanvasGroupOn(Quest);
+            CanvasGroupOn(UI_2);
             PanelNum++;
-        }
+        }    
+
         else if(PanelNum == 6)
         {
-            CanvasGroupOff(UI_4);
-            CanvasGroupOn(UI_5);
-            PanelNum++; // 지금 7
+            CanvasGroupOff(UI_2);
+            CanvasGroupOn(UI_4);
+            PanelNum++; 
         }
         else if(PanelNum == 7)
         {
-            CanvasGroupOff(UI_5);
-            CanvasGroupOn(UI_6);
-            PanelNum++; // 지금 7
+            CanvasGroupOff(UI_4);
+            CanvasGroupOn(UI_5);
+            CanvasGroupOff(stageUI);
+            PanelNum++; 
         }
         else if(PanelNum == 8)
         {
-            CanvasGroupOff(UI_6);
-            CanvasGroupOn(UI_7);
-            PanelNum++; // 지금 7
+            CanvasGroupOff(UI_5);
+            CanvasGroupOn(UI_6);
+            PanelNum++; 
         }
         else if(PanelNum == 9)
         {
+            CanvasGroupOff(UI_6);
+            CanvasGroupOn(UI_7);
+            PanelNum++; 
+        }
+        else if(PanelNum == 10)
+        {
             CanvasGroupOff(UI_7);
+            CanvasGroupOn(UI_8);
+            PanelNum++; 
+        }
+        else if(PanelNum == 11)
+        {
+            CanvasGroupOff(UI_8);
+            CanvasGroupOn(UI_9);
+            PanelNum++; 
+        }
+        else if(PanelNum == 12)
+        {
+            CanvasGroupOff(UI_9);
             CanvasGroupOn(joy);
             CanvasGroupOn(stageUI);
-            CanvasGroupOn(questIcon);
 
-            PanelNum++; // 지금 7
+            PanelNum++; 
             tutorial = 1;
             GameObject.Find("DataManager").GetComponent<DataManager>().datas.stage1Tutorial = tutorial;
             DataManager.instance.DataSave();
         }
+
+
     }
 
     // Update is called once per frame
