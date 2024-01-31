74bbed617faec47a48e8aabe68fc19c6a8884c8c
Sugedi <happy981215@naver.com>
2024-01-31 10:09:19 +0900

    New Function: 시작 컷신 삽입

--- Project_A/Assets/Scenes/System/Scipts_SH/Tutorial/IntroVideo.cs
+++ Project_A/Assets/Scenes/System/Scipts_SH/Tutorial/IntroVideo.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class IntroVideo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private double videoLength;
+    private float time;
     void Start()
     {
-        
+        videoLength = GameObject.Find("VideoPlay").GetComponent<VideoPlayer>().clip.length;
+        Debug.Log(videoLength);
+        StartCoroutine(GoToStage());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        time += Time.deltaTime;
+    }
+    IEnumerator GoToStage()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Backstage_0114");
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            yield return null;
+
+            if (videoLength <= time)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
     }
 }
