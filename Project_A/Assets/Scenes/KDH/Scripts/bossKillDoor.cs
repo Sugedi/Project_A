7b2d08dbf9d60560a4acb662bf70edd98172f54b
jeongmijang <wkdwjdal4355@gmail.com>
2024-02-01 22:26:00 +0900

    new function: 시네머신 카메라 추가
    갈리오 죽을시, 문열림 앵글

--- Project_A/Assets/Scenes/KDH/Scripts/bossKillDoor.cs
+++ Project_A/Assets/Scenes/KDH/Scripts/bossKillDoor.cs
@@ -1,62 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class bossKillDoor : MonoBehaviour
 {
-    public AnimationClip bossDoorOpenAnimation;
-    // ...
+    public AnimationClip bossDoorOpenAnimation; // 보스가 죽으면 문이 열리는 애니메이션
+
+    public CinemachineVirtualCamera mainVirtualCamera; // 메인카메라
+    public CinemachineBlendListCamera blendListCamera;  // 블랜드 리스트 카메라
+
     private void Start()
     {
+
+        // 기본적으로는 메인카메라 활성화
+        if (mainVirtualCamera != null)
+        {
+            mainVirtualCamera.enabled = true;
+        }
+
+        // 블렌딩 카메라는 비활성화 상태로 시작
+        if (blendListCamera != null)
+        {
+            blendListCamera.enabled = false;
+        }
+
+        // 만약 '갈리오'를 처음으로 클리어했다면
         if (DataManager.instance.datas.galioFirstClear == true)
         {
+            // 이 게임 오브젝트를 비활성화합니다.
             gameObject.SetActive(false);
         }
     }
+
     public void BossKillDoorOpen()
     {
-        // Check if the doorAnimation is assigned
+        // 만약 문의 애니메이션이 지정되어 있다면
         if (bossDoorOpenAnimation != null)
         {
-            // Play the 'bosskilldooropen' animation
+            // '보스를 죽이고 문을 열기' 애니메이션을 실행합니다.
             bossPlayDoorAnimation(bossDoorOpenAnimation);
         }
         else
         {
+            // 경고: 보스 킬 도어 스크립트에 문 애니메이션이 지정되지 않았습니다.
             Debug.LogError("Door Animation not assigned to BossKillDoor script.");
         }
     }
+
     void bossPlayDoorAnimation(AnimationClip animationClip)
     {
-        // 문 열림 사운드를 재생
+        // 문이 열릴 때 사운드를 재생합니다.
         SoundManager.instance.PlayAudio("Gate1", "SE");
 
-        // Set the specified animation clip to the Animation component
+        // Animation 컴포넌트에 지정된 애니메이션 클립을 설정합니다.
         GetComponent<Animation>().clip = animationClip;
 
-        // Play the animation once
+        // 애니메이션을 한 번 재생합니다.
         GetComponent<Animation>().wrapMode = WrapMode.Once;
         GetComponent<Animation>().Play();
-        // Add self-disable method call
+
+        // 문이 열리는 동안 블랜드 리스트 카메라를 활성화하고 메인 카메라를 비활성화
+        mainVirtualCamera.enabled = false;
+        blendListCamera.enabled = true;
+
+        // 애니메이션 재생 후에 스크립트를 비활성화하는 메서드를 호출합니다.
         StartCoroutine(DisableAfterAnimation(animationClip.length));
+
     }
 
     IEnumerator DisableAfterAnimation(float duration)
     {
-        // Wait for the animation to finish
+        // 애니메이션이 끝날 때까지 기다립니다.
         yield return new WaitForSeconds(duration);
 
-        // Disable the script or GameObject
+        // 문이 닫히는 동안 메인 카메라를 활성화하고 블랜드 리스트 카메라를 비활성화
+        mainVirtualCamera.enabled = true;
+        blendListCamera.enabled = false;
+
+        // 스크립트를 비활성화합니다.
         DisableScript();
     }
 
     void DisableScript()
     {
+        // '갈리오'를 처음으로 클리어했다고 표시합니다.
+        DataManager.instance.datas.galioFirstClear = true;
 
-            DataManager.instance.datas.galioFirstClear = true;
-            Debug.Log("Galio has been defeated!");
-            gameObject.SetActive(false);
-            DataManager.instance.DataSave();
-        
+        // 로그에 메시지를 남깁니다.
+        Debug.Log("갈리오를 물리쳤습니다!");
+
+        // 이 게임 오브젝트를 비활성화합니다.
+        gameObject.SetActive(false);
+
+        // 데이터를 저장합니다.
+        DataManager.instance.DataSave();
     }
 }
